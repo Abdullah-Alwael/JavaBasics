e219f25c4c3ccbeb3fbcfc622614170b6824a8b2
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-06-23 21:41:20 +0300

    final touches

--- src/JavaBasics.java
+++ src/JavaBasics.java
@@ -80,7 +80,7 @@ public class JavaBasics {
 
         int startofStringIndex = -1;
         do {
-            System.out.println("enter the start of the substring (integer number >=0<"+entireSentence.length());
+            System.out.println("enter the start of the substring (integer number >=0<"+entireSentence.length()+")");
             startofStringIndex = input.nextInt();
             if (startofStringIndex < 0 || startofStringIndex >=entireSentence.length()){
                 System.out.println("Cant be, enter again");
@@ -89,7 +89,7 @@ public class JavaBasics {
 
         int endofStringIndex = 1;
         do {
-            System.out.println("enter the end of the substring (integer number >"+startofStringIndex+"<"+entireSentence.length());
+            System.out.println("enter the end of the substring (integer number >"+startofStringIndex+"<"+entireSentence.length()+")");
             endofStringIndex = input.nextInt();
             if (endofStringIndex <= startofStringIndex || endofStringIndex >=entireSentence.length()){
                 System.out.println("Cant be, enter again");
@@ -146,7 +146,7 @@ public class JavaBasics {
         if (firstString.equalsIgnoreCase(secondString)){
             System.out.println("Strings are equal");
         } else {
-            System.out.println("String are not equal");
+            System.out.println("Strings are not equal");
         }
     }
 }
