b4d42d92784316a7a6993c26c83079c9f5f7845b
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-06-23 20:21:49 +0300

    Q3

--- src/JavaBasics.java
+++ src/JavaBasics.java
@@ -8,10 +8,12 @@ public class JavaBasics {
         * 1. Develop a program that takes the weight (in kilograms) and height (in meters)
              as input and calculates the BMI, then prints it.
         * */
-        System.out.println("Please enter your weight in Kilo Grams(KG): ");
+
+        System.out.println("BMI Calculator");
+        System.out.println("enter your weight in Kilo Grams(KG): ");
         float weight = input.nextFloat();
 
-        System.out.println("please enter your height in meters(m)");
+        System.out.println("enter your height in meters(m)");
         float height = input.nextFloat();
 
         float bmi = weight/(height*height);
@@ -23,13 +25,30 @@ public class JavaBasics {
         * */
 
         System.out.println("Grade percentage calculator");
-        System.out.println("please enter your obtained mark (number)");
+        System.out.println("enter your obtained mark (number)");
         float obtainedMarks = input.nextFloat();
 
-        System.out.println("Please enter the total marks (out of)");
-        float total = input.nextFloat();
+        System.out.println("enter the total marks (out of)");
+        float totalMarks = input.nextFloat();
 
-        float percentage = obtainedMarks/total;
-        System.out.println("Percentage = "+percentage);
+        float percentage = obtainedMarks/totalMarks*100;
+        System.out.println("Percentage = "+percentage+"%");
+
+        /*
+        *3. Create a program that takes an amount in one currency and an exchange rate
+            as input, then converts and prints the amount in another currency.
+        * */
+
+        System.out.println("Exchange rate calculator:");
+        System.out.println("Enter the amount in the original currency");
+        double amountInOriginalCurrency = input.nextDouble();
+
+        System.out.println("enter the exchange rate for the desired currency");
+        double desiredCurrencyExchangeRate = input.nextDouble();
+
+        System.out.println("Amount after rate exchange is:");
+
+        double desiredAmount = amountInOriginalCurrency*desiredCurrencyExchangeRate;
+        System.out.println(desiredAmount);
     }
 }
